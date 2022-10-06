d3afb662a99743f1e76a9a0e7117cbd1c80d4ba8
0mgfriday <0mgfriday@omg>
2022-10-06 00:00:18 -0400

    ps url encoding

--- profile.ps1
+++ profile.ps1
@@ -7,5 +7,13 @@ function b64d ($str) {
     [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("$str"))
 }
 
+function urlencode ($str) {
+    [System.Web.HTTPUtility]::UrlEncode($str)
+}
+
+function urldecode ($str) {
+    [System.Web.HTTPUtility]::UrlDecode($str)
+}
+
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineOption -ShowToolTips
